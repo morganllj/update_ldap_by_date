71706381848332abbfb5fa706f2c84e902d357d7
Morgan Jones <morgan@morganjones.org>
2018-12-12 14:01:03 -0500

    testing with ldap3, not working: centos ldap returns a referral for a search

--- update_ldap_by_date.py
+++ update_ldap_by_date.py
@@ -5,6 +5,7 @@ import getopt
 import re
 import sys
 import yaml
+from ldap3 import Server, Connection, ALL
 
 def print_usage():
     print ("usage: "+sys.argv[0]+" [-n] -c <config>.yml ")
@@ -24,8 +25,36 @@ for opt, arg in opts:
 if (config_file is None):
     print_usage()
 
-# https://martin-thoma.com/configuration-files-in-python/
+# https://martin-thoma.com/configuration-files-in-python/open
 with open (config_file, 'r') as ymlfile:
     config = yaml.load(ymlfile)
 
-print (config["ldap"]["user"])
+# print (config["ldap"]["user"])
+
+
+    
+# try:
+    # ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+    # l = ldap.initialize(config[ldap][host])
+    # l.simple_bind(config[ldap][binddn], config[ldap][bindpass]);
+    # r = l.search(config[ldap][basedn], ldap.SCOPE_SUBTREE, config[ldap][search]);
+    # result_set = []
+
+    # while 1:
+    #     result_type, result_data = l.result(r, 0)
+    #     if (result_data == []):
+    #         break
+
+    #     print ("result:", result_data)
+
+#server = Server(config["ldap"]["host"], get_info=ALL)
+server = Server(config["ldap"]["host"])
+conn = Connection(server, config["ldap"]["binddn"], config["ldap"]["bindpass"], auto_bind=True)
+rc = conn.search(config["ldap"]["basedn"], config["ldap"]["search"])
+print ("rc:", rc)
+print (conn.entries)
+
+
+
+# except ldap.LDAPError, e:
+#     print "problem searching ldap: ", e
