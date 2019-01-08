20d9a2d3f70e297963f592b315b84a373f4472d0
Morgan Jones <morgan@morganjones.org>
2019-01-08 16:33:40 -0500

    basically working, needs cleanup

--- update_ldap_by_date.py
+++ update_ldap_by_date.py
@@ -5,13 +5,54 @@ import getopt
 import re
 import sys
 import yaml
-from ldap3 import Server, Connection, ALL
+from ldap3 import Server, Connection, ALL, MODIFY_REPLACE, MODIFY_ADD
 from datetime import datetime, timezone, date, timedelta
 import pytz
 
 def print_usage():
     print ("usage: "+sys.argv[0]+" [-n] -c <config>.yml ")
     exit()
+
+
+def change_values(e):
+    i=0
+    changes = {};
+
+
+    if not print_only:
+        # conn.modify(e["dn"],
+        #     { cfg["ldap"]["change_attr"]:   [(MODIFY_REPLACE, ["ldap"]["change_value"])],
+        #       cfg["ldap"]["statuslogattr"]: [(MODIFY_ADD,     [ldapdatenow+" update_ldap_by_date.py set "+changefrom+" to "+changeto])]
+        #     })
+
+        conn.modify(e["dn"],
+            { cfg["ldap"]["change_attr"]:       [(MODIFY_REPLACE, cfg["ldap"]["change_value"])],
+              cfg["ldap"]["acctstatuslogattr"]: [(MODIFY_ADD,     [ldapdatenow+" update_ldap_by_date.py set "+cfg["ldap"]["change_attr"]+" to "+cfg["ldap"]["change_value"]])]
+            })        
+        if conn.result["result"]:
+            print ("modify failed:", conn.result)
+            sys.exit()
+
+        print("")
+        sys.exit()
+
+
+
+
+#        changes[cfg["ldap"]["acctstatusattr"]] = [(MODIFY_ADD, [ldapdatenow+" update_ldap_by_date.py set "+changefrom+" to "+changeto])]
+
+    
+
+    
+#    changes[cfg["ldap"]["acctstatusattr"]: [(MODIFY_ADD, [ldapdatenow+" update_ldap_by_date.py"])]]
+#     for a in cfg["ldap"]["change_attrs"]:
+# #        changes[cfg["ldap"]["change_attrs"][i]] =  [(MODIFY_REPLACE, [cfg["ldap"]["change_values"[i]]])]
+# #        changes["sdpForceAccountUpdate"] =  [(MODIFY_REPLACE, [cfg["ldap"]["change_values"[i]]])]
+#         changes["sdpForceAccountUpdate"] =  [(MODIFY_REPLACE, [)]
+#         i=i+1
+#     print ("changes:", changes)
+# #    conn.modify(dn, changes)
+    
 
 opts, args = getopt.getopt(sys.argv[1:], "nc:")
 
@@ -35,8 +76,13 @@ server = Server(cfg["ldap"]["host"])
 conn = Connection(server, cfg["ldap"]["binddn"], cfg["ldap"]["bindpass"], auto_bind=True)
 rc = conn.search(cfg["ldap"]["basedn"], cfg["ldap"]["search"], attributes=['*'])
 
+ # olddatetime=olddatetime.astimezone(pytz.UTC)
+ #        priorldapdateexp = olddatetime.strftime('%Y%m%d%H%M%S') + "Z"
+
+ #    ldapdatenow = datetime.utcnow().strftime('%Y%m%d%H%M%S') + "Z"
+
 datenow = datetime.now(pytz.utc);
-#print ("datenow:", datenow)
+ldapdatenow = datenow.astimezone(pytz.UTC).strftime('%Y%m%d%H%M%S') + "Z"
 
 for e in conn.response:
     print (e["attributes"]["uid"][0], "!", sep="", end="")
@@ -46,12 +92,12 @@ for e in conn.response:
         lookbacktime = timedelta(days=cfg["other"]["lookback"])
         if dateinldap < (datenow-lookbacktime):
             print ("forceupdate", end="")
-            # change_values(e)
+            change_values(e)
         else:
             print ("noupdate", sep="!", end="")
     else:
         print ("None","forceupdate", sep="!", end="")
-        #change_values(e)
+        change_values(e)
     print ("")
         
 
